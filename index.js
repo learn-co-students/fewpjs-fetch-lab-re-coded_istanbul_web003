d0218935c7b9a575b255a04ddb58b464598c5e3b
fadime94 <fadime.ozdemir11@gmail.com>
2020-07-26 16:42:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(json => renderBooks(json));
+  return fetch('https://anapioficeandfire.com/api/books')
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+  // Use this data inside of `json` to do DOM manipulation
+  return renderBooks(json);
+})
 }
 
 function renderBooks(books) {
