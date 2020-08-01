c20298abe6598743cc33120d28cceaacc9dfc974
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-08-01 21:25:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-    return fetch();
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
   .then(json => renderBooks(json));
-    
-  }
 
-function renderBooks(books) {
+
+
+}
+
+function renderBooks(json) {
   const main = document.querySelector('main')
-  books.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+    h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
