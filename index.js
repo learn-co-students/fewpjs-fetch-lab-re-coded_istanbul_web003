c60745941f34f4ef8c2e43c1b10a3f6e67de9847
Hiba <hiba.mashfij@gmail.com>
2020-08-04 13:03:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 function fetchBooks() {
 
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function (resp) {
+      return resp.json() })
+  .then(json => renderBooks(json))
+  
+ 
 }
 
-function renderBooks(books) {
+function renderBooks(json) {
   const main = document.querySelector('main')
-  books.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+fetchBooks()
